bd27b021e127216bfdf0624e8b28b2da6e697728
Keshav Malik <33570148+theinfosecguy@users.noreply.github.com>
2023-09-22 00:05:05 +0530

    Added Semgrep rules

---
+++ challenge1/no-console-log.js
@@ -0,0 +1,13 @@
+function greetUser(name) {
+    console.log("Hello, " + name);
+}
+
+function checkValue(num) {
+    if (num < 0) {
+        console.log("Negative number detected");
+    }
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log("Iteration " + i);
+}
---
+++ challenge3/challenge.py
@@ -0,0 +1,3 @@
+from os import *
+from semgrep import *
+from datetime import datetime
